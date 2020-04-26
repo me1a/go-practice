b55e7d32c509f9f48483d65f8c04f7c1dd318415
me1a <mxh4373@gmail.com>
2020-04-26 18:28:04 +0800

    type 补充

--- tour/type-inf.go
+++ tour/type-inf.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+func test() {
+	type flags string
+	// var f flags = "go"
+	// var s string = "go"
+	// fmt.Printf(f == s) // invalid operation: f == s (mismatched types flags and string)
+
+	type str = string
+	var ss str = "go"
+	var sss string = "go"
+	fmt.Println(ss == sss)
+}
+
 func main() {
 	v := 52
 	fmt.Printf("v is of type %T\n", v)
+	test()
 }
